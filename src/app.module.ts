002c3ad583009e9e01bb6d6f15ceb19655eb5ddb
Sneha Pal <Sneha1289pal@gmail.com>
2025-08-03 10:16:03 +0530

    feat(auth): implement JWT authentication with guards, strategies, and user decorators
    - Added JWT strategy, guards, and user decorators for authentication
    - Created DTOs for login and registration
    - Updated auth module, controller, and service to support JWT
    - Updated app module and dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,25 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { EventsModule } from './events/events.module';
 import { AlertsModule } from './alerts/alerts.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Bookmark, User, Event } from 'libs/database/entities';
 
 @Module({
-  imports: [AuthModule, UserModule, EventsModule, AlertsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [User, Bookmark, Event],
+    }),
+    AuthModule,
+    UserModule,
+    EventsModule,
+    AlertsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
