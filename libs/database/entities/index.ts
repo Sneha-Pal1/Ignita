bc302a464010783a399f49ffde881bed41d78880
Sneha-Pal1 <sneha1289pal@gmail.com>
2025-08-09 12:55:22 +0530

    Removed libs folder

--- libs/database/entities/index.ts
+++
@@ -1,3 +0,0 @@
-export * from '../../../src/auth/entities/user.entity';
-export * from '../../../src/events/entities/event.entity';
-export * from '../../../src/bookmark/entities/bookmark.entity';
--- libs/database/enums/index.ts
+++
@@ -1,3 +0,0 @@
-export * from '../../../src/auth/entities/user.entity';
-export * from '../../../src/events/enums/event-category.enum';
-export * from '../../../src/events/enums/event-type.enum';
